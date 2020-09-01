5f41be864e50da95f9f772d71f78c5e7301cd234
Romano dos Anjos <dosanjosromano@gmail.com>
2020-08-31 22:05:35 -0300

    correção header

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { LandingComponent } from "./landingPage/landing.component";
+import { LandingFormComponent } from './landing-form/landing-form.component';
 
 @NgModule({
-    declarations: [LandingComponent],
+    declarations: [LandingComponent, LandingFormComponent],
     imports: [CommonModule],
     exports:[LandingComponent]
 })
