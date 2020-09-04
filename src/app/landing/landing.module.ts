ed7084e003813b0ad743cea1e62f8460b3f5d436
breno39 <64810645+breno39@users.noreply.github.com>
2020-09-04 01:11:10 -0300

    correções de layout, formatação e transferido conteudo de landing-form

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { LandingComponent } from "./landingPage/landing.component";
-import { LandingFormComponent } from './landing-form/landing-form.component';
 import { HeaderComponent } from './landingPage/header/header.component';
 import { JumbotronComponent } from './landingPage/jumbotron/jumbotron.component';
 import { TribesComponent } from './landingPage/tribes/tribes.component';
@@ -12,7 +11,7 @@ import { PreSignupComponent } from './landingPage/pre-signup/pre-signup.componen
 import { FooterComponent } from './landingPage/footer/footer.component';
 
 @NgModule({
-    declarations: [LandingComponent, LandingFormComponent, HeaderComponent, JumbotronComponent, TribesComponent, ListTribesComponent, CarouselTribesComponent, PreSignupComponent, FooterComponent],
+    declarations: [LandingComponent, HeaderComponent, JumbotronComponent, TribesComponent, ListTribesComponent, CarouselTribesComponent, PreSignupComponent, FooterComponent],
     imports: [CommonModule],
     exports:[LandingComponent]
 })
